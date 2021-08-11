e6de62ed0f4515e3e8cac8f39808f5fdf2b8dca9
Patrick <patrickdiehl@lsu.edu>
2021-08-11 13:58:39 -0500

    Add std::for_each

---
+++ src/parallel_algorithms/main_for_each.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <random>
+#include <algorithm>
+#include <execution>
+
+int main(void){
+
+    size_t n = 10000;
+    // Generate the vector with the length
+    std::vector<double> v = std::vector<double>(n);
+    // Initilaize the vector with -1
+    std::generate(v.begin(),v.end(),std::rand);
+    // Fill a new vector contaning the indicies
+    std::vector<size_t> i = std::vector<size_t>(n);
+    std::iota(std::begin(i), std::end(i), 0);
+
+    std::for_each(
+    std::execution::par,
+    i.begin(),
+    i.end(),
+    [&](auto&& item)
+    {
+        v[item] = std::sqrt(v[item]);
+    });
+    
+
+    return EXIT_SUCCESS;
+
+ }
