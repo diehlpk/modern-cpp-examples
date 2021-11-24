37fca7380e5713b58c5a2bd09894af5af8c52b77
Patrick <patrickdiehl@lsu.edu>
2021-11-23 20:35:04 -0600

    Code quality

--- Docker/generate.sh
+++ Docker/generate.sh
@@ -3,4 +3,4 @@
 sudo podman build --tag docker.io/diehlpk/modern-cpp-base:latest -f ./Dockerfile
 sudo podman login docker.io
 id=$(sudo podman inspect --format="{{.Id}}" docker.io/diehlpk/modern-cpp-base:latest)
-sudo podman push $id docker://diehlpk/modern-cpp-base:latest
+sudo podman push "$id" docker://diehlpk/modern-cpp-base:latest
